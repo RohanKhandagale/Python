238fd3dd2b04c0272dd8e5504432af7ac3ddc3c2
Rohan K <63540808+RohanKhandagale@users.noreply.github.com>
2025-04-02 19:49:09 +0530

    Add files via upload

---
+++ Day6/Function.py
@@ -0,0 +1,9 @@
+# Write a function and call it
+
+# here we define our function
+def user_name ():
+    print("Hi there!")
+    name = input("What is your Name?\n")
+    print("Hi "+name)
+# here we call our function
+user_name()
