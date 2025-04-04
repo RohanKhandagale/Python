8137dea45a8eb924383ac98f46e77f4509244910
Rohan K <63540808+RohanKhandagale@users.noreply.github.com>
2025-04-04 16:14:35 +0530

    Update HurdleChallenge.py

--- Day6/HurdleChallenge.py
+++ Day6/HurdleChallenge.py
@@ -24,3 +24,25 @@ turn_around()
 turn_around()
 turn_around()
 turn_around()
+
+# Reeborg has entered a hurdle race, but he does not know in advance how long the race is.
+# Make him run the course, following a path similar to the one shown, 
+# but stopping at the only flag that will be shown after the race has started.
+# Hurdle2 solution
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def turn_around():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while not at_goal():
+    turn_around()
