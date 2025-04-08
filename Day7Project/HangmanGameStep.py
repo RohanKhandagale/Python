663d0afd90c335b1a51057658ffa61113e36b581
Rohan K <63540808+RohanKhandagale@users.noreply.github.com>
2025-04-08 12:19:28 +0530

    Update HangmanGameStep.py

--- Day7Project/HangmanGameStep.py
+++ Day7Project/HangmanGameStep.py
@@ -5,7 +5,7 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess the letter:-").lower()
+guess = input("Guess a letter:-").lower()
 print(guess)
 
 # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
