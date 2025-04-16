3d0af3578cb5fb0cbc1c44f98311ea9ccb36a02e
Rohan K <63540808+RohanKhandagale@users.noreply.github.com>
2025-04-16 16:09:26 +0530

    Add files via upload

---
+++ Day8/fuction.py
@@ -0,0 +1,14 @@
+def greet():
+    print("Hi")
+    print("Good Morning")
+    print("How are You?")
+
+greet()
+# Function with inputs
+
+def greet_with_name(name):
+    print(f"Hi {name}")
+    print(f"Good Morning {name}")
+    print(f"How are You? {name}")
+
+greet_with_name("Rohan")
