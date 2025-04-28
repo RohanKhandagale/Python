a419e0e7ce588dc6edd5ab1205590f1e4b8bc783
Rohan K <63540808+RohanKhandagale@users.noreply.github.com>
2025-04-28 16:32:33 +0530

    Add files via upload

---
+++ LoanEligibilityCheck/LoanEligibility.py
@@ -0,0 +1,8 @@
+# Ask for the salary
+salary = float(input("Enter your monthly salary: "))
+
+# Check loan eligibility
+if salary >= 5000:
+    print("You are eligible for the loan.")
+else:
+    print("Sorry, you are not eligible for the loan.")
