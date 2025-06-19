9a5ce6e9e0516ca23a46aae2a4efde360089fa77
Rohan K <63540808+RohanKhandagale@users.noreply.github.com>
2025-06-19 17:05:01 +0530

    Add files via upload

---
+++ Practice/CodeChef.py
@@ -0,0 +1,47 @@
+# Ques. Ques 5. Right -Angled Triangle Pattern(left - Aligned)
+# a = 5
+# for i in range(1, a + 1):
+#     print("a" * i)
+
+# print(8)
+# print(13, end=" ")
+# print(21)
+
+# # ques 2. Square print
+# for i in range(1,100):
+#     print(f"{i}-{i*i}")
+
+# Ques 3. Square of star
+# a = 4
+# for i in range(a):
+#     print("*" * a)
+
+# x = 20
+# y = 6
+# print(x//y)
+
+#Ques 4. Area and Perimeter of Square
+# Write a program to find the area and perimeter of square whose side length is 4.5. On the first line,
+# print square's area and on the second line, its perimeter.
+#
+# Expected output
+# 20.25
+# 18.0
+# Note:
+# Area of a Square = side × side
+# # and, Perimeter = 4 x side
+#
+# side = 4.5
+# Area_of_a_Square = side * side
+# Perimeter = 4 * side
+# print(Area_of_a_Square)
+# print(Perimeter)
+
+
+
+# # Ques 5. Right -Angled Triangle Pattern(Right - Aligned)
+# a = 7
+# for i in range(1 , a + 1):
+#     print(" " * (a - i) + "*" * i)
+
+
